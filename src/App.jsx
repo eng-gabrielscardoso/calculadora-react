50920d2395827eaf0e9e76748563677c25b0dc99
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-25 16:32:26 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import Calculator from './layout/Calculator/Calculator';
+
+export default class App extends Component {
+  render () {
+    return (
+      <>
+        <Calculator />
+      </>
+    );
+  }
+}
