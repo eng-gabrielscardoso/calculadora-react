93fbab4878f25c4c9edfde0fe75d316de7a2fd4e
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-26 10:30:23 -0300

    feat: finish calculator logic

--- src/layout/Calculator/Calculator.jsx
+++ src/layout/Calculator/Calculator.jsx
@@ -28,19 +28,19 @@ export default class Calculator extends Component {
       return;
     }
 
-    const clearDisplay = this.state.displayValue === '0'
+    let clearDisplay = this.state.displayValue === '0'
       || this.state.clearDisplay;
 
-    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    let currentValue = clearDisplay ? '' : this.state.displayValue;
 
-    const displayValue = currentValue + digit;
+    let displayValue = currentValue + digit;
 
     this.setState({ displayValue, clearDisplay: false });
 
     if (digit != '.') {
-      const i = this.state.current;
-      const newValue = parseFloat(displayValue);
-      const values = [ ...this.state.values ];
+      let i = this.state.current;
+      let newValue = parseFloat(displayValue);
+      let values = [ ...this.state.values ];
 
       values[i] = newValue;
       
@@ -53,7 +53,34 @@ export default class Calculator extends Component {
   }
 
   setOperation (operation) {
-    console.log(operation);
+    if (this.state.current === 0) {
+      this.setState({ 
+        operation, 
+        current: 1, 
+        clearDisplay: true 
+      });
+    } else {
+      let equals = operation === '=';
+      let currentOperation = this.state.operation;
+
+      let values = [ ...this.state.values ];
+      
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+
+      this.setState({ 
+        displayValue: values[0], 
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      });
+    }
   }
 
   render () {
